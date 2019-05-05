a84bd05249f6cf1004fbb97f32be92426ac53ef0
Polis Flatt <30891943+polisflatt@users.noreply.github.com>
2019-05-05 17:34:22 -0500

    Add files via upload

--- src/lib/ip.php
+++ src/lib/ip.php
@@ -1,31 +1,35 @@
 <?php
-    require dirname(__FILE__)."/settings.php";
+    require dirname(__FILE__)."/settings.php"; /* Because relative fucking paths are a pain in PHP! */
 
-    function ip_address()
+    function ip_address() /* Get IP Address */
     {
         return isset($_SERVER['HTTP_CLIENT_IP']) ? $_SERVER['HTTP_CLIENT_IP'] : isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR'];
     }
 
-    function ip_information_curl($ip_address)
-    {
+    function ip_information_curl($ip_address) /* I don't know why it's still named curl... */
+    { /* Gets information from an IP query website. I use ipinfo.io, but you can use something different. Be prepared to change some code though... */
         $fp = file_get_contents($GLOBALS["ip_website"] . $ip_address);
         return $fp;
     }
 
+    /* Add IP to the log */
     function log_ip($ip_address)
     {
         $str_date = date($GLOBALS["date_format"], time());
 
+
         $log_folder_new = $GLOBALS["log_folder"] . $str_date . "/";
 
+        /* JSON Array */
         $json_array = array (
-            "ip" => (string) $ip_address,
-            "time" => (string) time()
+            "ip" => (string) $ip_address, /* Our IP Address */
+            "time" => (string) time() /* The time at which it was logged */
         );
 
 
         if (!file_exists($log_folder_new))
         {
+            /* If our logs don't already exist.*/
             mkdir($log_folder_new);
         }
 
@@ -35,22 +39,24 @@
         {
             $json_input = array();
         }
+        /* Poor way to add to the JSON array */
 
         array_push($json_input, $json_array);
 
         $fp = fopen("$log_folder_new/log.json", "w");
-        fwrite($fp, json_encode($json_input, JSON_PRETTY_PRINT) . PHP_EOL);
+        fwrite($fp, json_encode($json_input, JSON_PRETTY_PRINT) . PHP_EOL); /* Pretty print is a waste of space, but I keep it there for debugging purposes. */
         fclose($fp);
     }
 
 
 
-    
+    /* Create a nice addition to a preexisting table with table headers with the values here */
     function format_ip_html($ip_address, $time = 0)
     {
         $json_raw = ip_information_curl($ip_address);
         $json = json_decode($json_raw, true);
         $formatted_time = date($GLOBALS["time_format"], $time);
+        /* Get our information */
 
         printf(
             "<tr>\n"
@@ -62,7 +68,7 @@
 
         foreach ($json as $item) 
         {
-            
+            /* Lazily print each item */
             printf(
                 "<td>$item</td>\n"
             );
@@ -75,6 +81,7 @@
 
     }
 
+    /* Get all of our logs */
     function print_logs($date)
     {
         $json_content = json_decode(file_get_contents("src/logs/$date/log.json"), true);
--- src/lib/settings.php
+++ src/lib/settings.php
@@ -1,6 +1,11 @@
 <?php
+    /* Self explanatory? */
+
     $GLOBALS["ip_website"] = "http://ipinfo.io/";
     $GLOBALS["date_format"] = "Y-m-d";
     $GLOBALS["time_format"] = "H:i:s";
-    $GLOBALS["log_folder"] = dirname(__FILE__, 2) . "/logs/";
+    $GLOBALS["log_folder"] = dirname(__FILE__, 2) . "/logs/"; /* Ugh... 1+ point for Python. PHP's relative paths are a pain in the fucking ass. */
+
+    /* To be honest here, I see the faults of PHP. No standard library system seems strange and weird, whereas python has many libraries to choose from. You also have PIP. I tried getting PHP's package manager, but I couldn't figure out how to get it to work for hours on end. Absolute disaster. */
+    /* Still. PHP is so easy to use that I believe it is almost not necessary. */
 ?>
